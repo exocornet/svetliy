fb315b6d8115ceedb55d76390900f579bac8d0ec
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-09-16 23:01:31 +0300

    mobile 959

--- src/page/index/elemental/elemental.js
+++ src/page/index/elemental/elemental.js
@@ -9,25 +9,25 @@ $('.slider-elemental').slick({
   nextArrow: '<svg width="66" class="slick-next" height="16" viewBox="0 0 66 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M65.7071 7.29289C66.0976 7.68342 66.0976 8.31658 65.7071 8.70711L59.3431 15.0711C58.9526 15.4616 58.3195 15.4616 57.9289 15.0711C57.5384 14.6805 57.5384 14.0474 57.9289 13.6569L63.5858 8L57.9289 2.34315C57.5384 1.95262 57.5384 1.31946 57.9289 0.928932C58.3195 0.538408 58.9526 0.538408 59.3431 0.928932L65.7071 7.29289ZM0 7H65L65 9H0V7Z" fill="currentColor"/></svg>',
   responsive: [
     {
-      breakpoint: 1109,
+      breakpoint: 1169,
       settings: {
         centerPadding: '0px',
       }
     },
     {
-      breakpoint: 929,
+      breakpoint: 959,
+      settings: {
+        centerPadding: '5px',
+      }
+    },
+    {
+      breakpoint: 769,
       settings: {
 
       }
     },
     {
-      breakpoint: 767,
-      settings: {
-
-      }
-    },
-    {
-      breakpoint: 575,
+      breakpoint: 579,
       settings: {
 
       }
