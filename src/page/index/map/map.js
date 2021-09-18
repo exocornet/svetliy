cd680482777bac62b98aec81d73d2790db02099a
AntoshJkeee <toha_bart@mail.ru>
2021-09-18 03:18:38 +0300

    добил все кроме  формы

--- src/page/index/map/map.js
+++ src/page/index/map/map.js
@@ -1,4 +1,3 @@
-
 function computedIndention() {
 
   let containerElement = document.querySelector('.container')
@@ -14,3 +13,53 @@ function computedIndention() {
 }
 
 computedIndention()
+
+ymaps.ready(init);
+function init(){
+
+  let myMap = new ymaps.Map("mymap", {
+    center: [55.13,83.00],
+    zoom: 11,
+    controls: ['fullscreenControl'],
+    behaviors: ['drag', 'dblClickZoom', 'multiTouch']
+  });
+
+  let multiRoute = new ymaps.multiRouter.MultiRoute({
+    // Точки маршрута.
+    referencePoints: [
+      [55.025927962966,82.92174633473742],
+      [55.06018602241668,82.91290969577021],
+      [55.195775638651995,83.14488700000003],
+    ]
+  }, {
+    // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+    boundsAutoApply: true,
+    wayPointStartIconColor: "#FFFFFF",
+    wayPointVisible: false
+  });
+
+  let placemark = new ymaps.Placemark([55.025927962966,82.92174633473742], {
+    hintContent: '<strong>Православный храм<strong>\n' +
+      'Красный просп., 17А, Новосибирск, Россия',
+    balloonContent: 'Православный храм, <br> Красный просп., 17А, Новосибирск, Россия',
+    iconCaption: 'Часовня Николая Чудотворца',
+  });
+
+  let placemark2 = new ymaps.Placemark([55.06018602241668,82.91290969577021], {
+    hintContent: 'Площадь имени Калинина<br>Метро Заельцовская, Новосибирск, Россия',
+    balloonContent: 'Площадь имени Калинина, Новосибирск, Россия',
+    iconCaption: 'Метро Заельцовская',
+  });
+
+  let placemark3 = new ymaps.Placemark([55.19042011727574,83.13975243127996], {
+    hintContent: 'п. Октябрьский, ул. Локтинская, д. 10/2, Новосибирск Россия',
+    balloonContent: 'Россия, Новосибирская область, Мошковский район, посёлок Октябрьский, микрорайон Светлый',
+    iconCaption: 'Жилой район "Светлый"',
+  });
+
+  myMap.geoObjects.add(placemark);
+  myMap.geoObjects.add(placemark2);
+  myMap.geoObjects.add(placemark3);
+  myMap.geoObjects.add(multiRoute);
+}
+
