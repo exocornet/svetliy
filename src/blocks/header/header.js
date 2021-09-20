dad698195838de2b3afe181d75273013ad579733
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-09-20 21:47:40 +0300

    изменение меню хедера

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,19 +1,19 @@
 function burgerMenu() {
   let buttonBurger = document.querySelector('.burger');
-  let menu = document.querySelector('.menu');
+  let menu = document.querySelector('.header__wrap-menu');
   let menuLink = document.querySelectorAll('.menu__link');
   let body = document.querySelector('body')
 
   buttonBurger.addEventListener('click', () => {
     buttonBurger.classList.toggle('burger_active');
-    menu.classList.toggle('menu_active');
+    menu.classList.toggle('header__wrap-menu_active');
     body.classList.toggle('overflow-hidden');
   });
 
   menuLink.forEach(item => {
     item.addEventListener('click', () => {
       buttonBurger.classList.remove('burger_active');
-      menu.classList.remove('menu_active');
+      menu.classList.remove('header__wrap-menu_active');
       body.classList.remove('overflow-hidden');
     });
   });
